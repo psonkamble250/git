88d72fe8592115176cd28c99bb3fea4be3b70eec
PriyankaS <psonkamble250@gmail.com>
2022-01-20 11:48:34 +0530

    multiply commit

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package com.ycp;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		x=10;
+		y=20;
+		System.out.println("multiply"+x*y);//here + is use to concatinnate
+		}
+}
